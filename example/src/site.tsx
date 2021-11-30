50963d87f9084fe6d0bbbd787d0c47682a5f9666
Stephen Blackstone <sblackstone@gmail.com>
2021-11-30 15:14:48 -0500

    latest

--- example/src/site.tsx
+++ example/src/site.tsx
@@ -5,10 +5,15 @@ import { DebugComponent, CartDisplay, ToggleAddonButton } from 'rtc-react';
 export const Site = () => {
   return (
     <>
+      <h3>Cart Dispaly</h3>
       <CartDisplay />
+      <hr/>
+      <h3>Toggle Addon Buttons</h3>
       <ToggleAddonButton text="small addon" variantId="39518515298490" />
       <ToggleAddonButton text="medium addon" variantId="39518515331258" />
       <ToggleAddonButton text="large addon" variantId="39518515364026" />
+      <hr/>
+      <h3>Debug View</h3>
       <DebugComponent />
     </>
   );

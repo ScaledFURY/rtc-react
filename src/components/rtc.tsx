de1172ad26430e65ce0a78b0400f3a5b2c10374c
Stephen Blackstone <sblackstone@gmail.com>
2021-12-03 07:17:48 -0500

    More documentation

--- src/components/rtc.tsx
+++ src/components/rtc.tsx
@@ -31,7 +31,35 @@ export interface IRTCProps {
 
 
 
-/* Main wrapper of RTC Library */
+/**
+  Main wrapper of RTC Library
+
+  The user passes a <Component> that should be the main entry point to the lander.
+
+  Example:
+
+  ```
+  <RTC component={Site}
+       defaultVariantId="40194513993914"
+       defaultAddons="39518515298490"
+       forceVariantId="39507333480634"
+       nextUrl="https://www.whatever.com"
+       apiEndpoint="api.burnerdomain.rtccart.io"
+       landingPageName="LPN"
+       upsellPageName="UPN"
+       funnelName="FN"
+       orderTag="OT"
+       advertorialPageName="APN"
+       trackStock={false}
+       paypalConfirmUrl="http://www.whatever.com"
+       eventHandler={eh}
+       isCheckoutPage={false}
+  />
+  ```
+
+
+
+*/
 export const RTC = (props: IRTCProps) => {
   //console.log("Starting RTC Render");
   ApiClient.setApiEndpoint(props.apiEndpoint);

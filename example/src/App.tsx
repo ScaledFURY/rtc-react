ee35c38ce5549be85231e393a9df7ff52deba576
Stephen Blackstone <sblackstone@gmail.com>
2021-12-08 14:34:16 -0500

    Latest

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
-import { RTC } from 'rtc-react'
-import { Site } from "./site";
+import { RTC, CartDisplay } from 'rtc-react'
+
+const Site : any = (props:any) => {
+  React.useEffect(() => {
+      props.rtcApi.loadCart({
+        defaultVariantId: "40194513993914",
+        defaultAddons: "39518515298490"
+      });
+
+  }, [ props.rtcApi]);
+
+  return (
+    <CartDisplay />
+  );
+
+}
 
 const App = () => {
 
-  const eh = async(e:any, rtcApi:any) => {
+  const eventHandler = async(e:any) => {
     console.log(`custom event handler received: ${e.eventType}`);
-    rtcApi.normalizedTimestamp(); // keep TS compiler happy about otherwise unused rtcApi.
   };
 
   return (
     <RTC component={Site}
-//         forceVariantId="39507333480634"
          nextUrl="https://www.whatever.com"
          apiEndpoint="api.burnerdomain.rtccart.io"
-//         landingPageName="LPN"
-//         upsellPageName="UPN"
-//         funnelName="FN"
-//         orderTag="OT"
-//         advertorialPageName="APN"
-//         trackStock={false}
-//         paypalConfirmUrl="http://www.whatever.com"
-         eventHandler={eh}
-//         isCheckoutPage={false}
+         eventHandler={eventHandler}
     />
   );
 }

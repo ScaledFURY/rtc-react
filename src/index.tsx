023ea8c96b020be9a83e4aa90b846b12248b3f21
Stephen Blackstone <sblackstone@gmail.com>
2021-12-14 18:09:16 -0500

    Put back an export for doc purposes

--- src/index.tsx
+++ src/index.tsx
@@ -33,3 +33,4 @@ export { QuantitySelector } from "./components/quantity_selector";
 export { CountrySelectOptions } from "./components/country_select_options";
 export { StateSelectOptions } from "./components/state_select_options";
 export { PaypalCheckoutButton } from "./components/paypal_checkout_button";
+export * as RtcApi from "./rtc_api";
